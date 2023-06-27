2b89132896b0fee872254641f9fcd400a0774e7c
vym07 <veselov.jarik@gmail.com>
2023-06-27 11:08:44 +0300

    ti 10-11

--- js/script.js
+++ js/script.js
@@ -25,8 +25,10 @@ const skills = {
         level: 40,
         iconPath: "img/skills/english.svg",
     }],
+    isSorted: false,
     generateList: function(parentElement) {
-        this.data.forEach(element=> {
+        parentElement.innerHTML = '';
+        this.data.forEach(element => {
             const dt = document.createElement('dt');
             dt.classList.add('skill-item');
             dt.textContent = element.item;
@@ -42,7 +44,81 @@ const skills = {
             dd.append(div);
             parentElement.append(dt, dd);
         });
-    }
+    },
+    sortList: function(sortingType) {
+        if (skills.isSorted !== sortingType) {
+            switch (sortingType) {
+                case 'name':
+                    this.data.sort((a, b) => a.item.localeCompare(b.item)); break;
+                case 'level':
+                    this.data.sort((a, b) => b.level - a.level); break;
+                default:
+                    return;
+            }
+            console.log(`отсортировано по ${sortingType}`);
+            this.isSorted = sortingType;
+        } else {
+            this.data.reverse();
+            console.log('инвертирован порядок сортировки');
+        }
+        this.generateList(skillListSelector);
+    },
 };
 
-skills.generateList(document.querySelector('dl.skill-list'));
+const skillListSelector = document.querySelector('dl.skill-list');
+
+// Меню навигации: .main-header
+const menu = {
+    open: function() {
+        nav.classList.remove('main-nav_closed');
+        btn.classList.add('nav-btn_close');
+        btn.classList.remove('nav-btn_open');
+        btn.innerHTML = '<span class="visually-hidden">Закрыть меню</span>';
+    },
+    close: function() {
+        nav.classList.add('main-nav_closed');
+        btn.classList.remove('nav-btn_close');
+        btn.classList.add('nav-btn_open');
+        btn.innerHTML = '<span class="visually-hidden">Открыть меню</span>';
+    },
+};
+
+// Первое создание списка навыков
+skills.generateList(skillListSelector);
+
+// Сортировки списка по кнопкам
+const sortBtnsBlock = document.querySelector('.skills-buttons');
+sortBtnsBlock.addEventListener('click', (e) => {
+    if (e.target.nodeName === "BUTTON") {
+        skills.sortList(e.target.dataset.type);
+    }
+});
+
+// Показ и скрытие меню (гамбургер)
+const nav = document.querySelector('.main-nav');
+const btn = document.querySelector('.nav-btn');
+btn.addEventListener('click', (e) => {
+    e.target.classList.contains('nav-btn_open') ? menu.open() : menu.close();
+});
+menu.close();
+
+// Чекбокс темной темы
+const changeTheme = (theme) => {
+    theme
+      ? document.body.classList.remove('dark-theme')
+      : document.body.classList.add('dark-theme');
+    localStorage.setItem('dark-theme-disabled', theme);
+}
+const checkbox = document.querySelector(".switch-checkbox");
+checkbox.addEventListener("change", (e) => {
+    changeTheme(checkbox.checked);
+});
+// Загрузка сохраненной темы
+const darkThemeOnLoad = localStorage.getItem('dark-theme-disabled');
+if (darkThemeOnLoad === null) {
+    changeTheme(false);
+    checkbox.checked = false;
+} else {
+    changeTheme(darkThemeOnLoad === "true");
+    checkbox.checked = (darkThemeOnLoad === "true");
+}
